3fa9e887973565c906073f8de297211cc879250e
Callista Winegar-Valdez <cwvcallico@gmail.com>
2016-08-09 13:46:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-/* When the user clicks on the button, 
-  toggle between hiding and showing the dropdown-content. */
-  function myFunction() {
-    document.getElementById("myDropDown").classList.toggle("show");
-  }
-// Close the dropdown menu if user clicks outside of it
+/* When the user clicks on the button,
+ toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content")
-    var i;
-    for (i=0; i<dropdowns.length; i++) {
-      var openDropDown = dropdowns[i];
-      if (openDropDown.classList.contains('show')) {
-        openDropDown.classList.remove('show')
-      }
+    if (!event.target.matches('.dropbtn')) {
+        
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 }
