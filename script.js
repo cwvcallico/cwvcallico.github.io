6eaf05fef2537d7869609abb34e375426c338072
Callista Winegar-Valdez <cwvcallico@gmail.com>
2016-08-09 13:24:38 -0600

    adding animation to button

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
   function myFunction() {
     document.getElementById("myDropDown").classList.toggle("show");
   }
+// Close the dropdown menu if user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content")
+    var i;
+    for (i=0; i<dropdowns.length; i++) {
+      var openDropDown = dropdowns[i];
+      if (openDropDown.classList.contains('show')) {
+        openDropDown.classList.remove('show')
+      }
+    }
+  }
+}
